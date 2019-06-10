1ba06f0dd14c7b06a69956b752a38ca56349b05a
Ryan Littlefield <ryan.j.littlefield@gmail.com>
2019-06-10 17:20:40 +0100

    Add files via upload

---
+++ App_Main.py
@@ -0,0 +1,50 @@
+import sys
+import logging
+from flask import json
+from flask import request
+from flask import Flask
+from pyngrok import ngrok, process
+from logging.handlers import RotatingFileHandler
+
+#Debug Error Handling for Process Initiation
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
+
+#Defining variables to build Ngrok tunnel
+## This overrides the default ngrok config and builds based on params below
+NAME = "WebHooks"
+CONFIG_PATH = "/Users/Ryan/.ngrok2/ngrok.yml"
+SUBDOMAIN = {
+    "subdomain": "humio"
+}
+ngrok.connect(port=5000, name=NAME, config_path=CONFIG_PATH, options=SUBDOMAIN)
+tunnels = ngrok.get_tunnels()
+print(tunnels)
+
+app = Flask(__name__)
+
+@application.route('/', methods=['POST'])
+def get_payload():
+    if request.headers['content-type'] == 'application/json':
+        log = json.dumps(request.json)
+        with open('logs.json', 'w') as f:
+            json.dump(request.json, f)
+        return(log)
+
+@app.route('/')
+def error():
+    app.logger.warning('A warning occurred (%d total)')
+    app.logger.error('An error occurred')
+    app.logger.info('Info')
+    return(error)
+
+if __name__ == '__main__':
+    handler = RotatingFileHandler('error.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
+    app.run()
---
+++ WSGI_Server_Main.py
@@ -0,0 +1,4 @@
+from Hmac_Verify import application
+
+if __name__ == "__main__":
+    application.run()
