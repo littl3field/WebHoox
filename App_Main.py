30a7b4034f97f59f2a19cabeada61b6dd1afbb23
Ryan Littlefield <ryan.j.littlefield@gmail.com>
2019-06-10 17:30:29 +0100

    Update App_Main.py

--- App_Main.py
+++ App_Main.py
@@ -1,10 +1,16 @@
+import os
 import sys
-import logging
+from pyngrok import ngrok, process
 from flask import json
 from flask import request
 from flask import Flask
-from pyngrok import ngrok, process
+import logging
 from logging.handlers import RotatingFileHandler
+import hmac
+import hashlib
+
+#Define application
+application = Flask(__name__)
 
 #Debug Error Handling for Process Initiation
 root = logging.getLogger()
@@ -17,34 +23,48 @@ root.addHandler(handler)
 
 #Defining variables to build Ngrok tunnel
 ## This overrides the default ngrok config and builds based on params below
-NAME = "WebHooks"
-CONFIG_PATH = "/Users/%USER%/.ngrok2/ngrok.yml" #Change User to your username
+NAME = "RACSN_WebHooks"
+CONFIG_PATH = "/Users/%USERNAME%/.ngrok2/ngrok.yml" #Change %USERNAME% to your username
 SUBDOMAIN = {
-    "subdomain": "TESTCHANGEME" #Change this to whatever domain you have set
+    "subdomain": "CHANGEME" #Change this to whatever domain you require 
 }
+
+#Build Ngrok Tunnel
 ngrok.connect(port=5000, name=NAME, config_path=CONFIG_PATH, options=SUBDOMAIN)
 tunnels = ngrok.get_tunnels()
 print(tunnels)
 
-app = Flask(__name__)
+#Verify hash stored in environment variable
+def verify_token_hash(data, signature):
+    token = bytes(os.environ['WEBHOOK_TOKEN'], 'UTF-8')
+    mac_hash = hmac.new(token, msg=data, digestmod=hashlib.sha1)
+    return hmac.compare_digest('sha1=' + mac_hash.hexdigest(), signature)
 
+#Set application route
 @application.route('/', methods=['POST'])
 def get_payload():
-    if request.headers['content-type'] == 'application/json':
-        log = json.dumps(request.json)
-        with open('logs.json', 'w') as f:
-            json.dump(request.json, f)
-        return(log)
+    signature = request.headers.get('X-Hub-Signature')
+    data = request.data
+    if verify_token_hash(data, signature):
+        if request.headers['content-type'] == 'application/json':
+            log = json.dumps(request.json)
+            with open('logs.json', 'w') as f:
+                json.dump(request.json, f)
+            return(log)
+    else:
+        return("Invalid Hash")
 
-@app.route('/')
+#Set logging on application route
+@application.route('/')
 def error():
-    app.logger.warning('A warning occurred (%d total)')
-    app.logger.error('An error occurred')
-    app.logger.info('Info')
+    application.logger.warning('A warning occurred (%d total)')
+    application.logger.error('An error occurred')
+    application.logger.info('Info')
     return(error)
 
+#Run Application
 if __name__ == '__main__':
     handler = RotatingFileHandler('error.log', maxBytes=10000, backupCount=1)
     handler.setLevel(logging.INFO)
-    app.logger.addHandler(handler)
-    app.run()
+    application.logger.addHandler(handler)
+    application.run()
