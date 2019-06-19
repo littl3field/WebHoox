e590518633c446d01ac1cd6893f1c4a90381e7b0
Ryan Littlefield <ryan.j.littlefield@gmail.com>
2019-06-19 15:51:11 +0100

    Update App_Main.py

--- App_Main.py
+++ App_Main.py
@@ -51,6 +51,7 @@ def get_payload():
             path = '/home/logs.json'
             with open(path, 'a') as f:
                 json.dump(request.json, f)
+                f.write("
             return(log)
     else:
         return("Invalid Hash")
